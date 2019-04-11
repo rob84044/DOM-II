57e9af5638c0b4a12486aff67af29dfee539b4ba
Robert Pedersen <rob84044@users.noreply.github.com>
2019-04-10 22:53:08 -0600

    Completed the 7th eventlistener

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 // Mouseover actions for the header and nav components
-
+//Upon entering the header of the site, site the header to orange with a default text color of white and when hovered over the title and the nav items will change to orange.
 //Set the variables
 let header = document.querySelector(".main-navigation");
 let logoHeading = document.querySelector(".logo-heading");
@@ -9,9 +9,6 @@ let navLink1 = document.getElementsByTagName("a")[0];
 let navLink2 = document.getElementsByTagName("a")[1];
 let navLink3 = document.getElementsByTagName("a")[2];
 let navLink4 = document.getElementsByTagName("a")[3];
-
-
-//Upon entering the header of the site, site the header to orange with a default text color of white and when hovered over the title and the nav items will change to orange.
 
 header.addEventListener('mouseover', function(event) {   
   // Highlight the header orange
@@ -23,7 +20,7 @@ header.addEventListener('mouseover', function(event) {
   navLink2.style.color = 'black';
   navLink3.style.color = 'black';
   navLink4.style.color = 'black';
-  })
+  });
 
 
 // When the main picture is clicked have an alert pop up that is populated with the images alt text
@@ -34,12 +31,56 @@ header.addEventListener('mouseover', function(event) {
   busImage.addEventListener('click', function(event) {
   window.alert(`${busImageAlt}`)
   event.stopPropagation()
+});
+
+
+// When you use the scroll wheel within the first div, its background turns orange
+  let divSelector = document.querySelector('.content-section .text-content p');
+
+  divSelector.addEventListener('click', function(event) {
+  event.target.style.backgroundColor = 'orange'; 
+  event.stopPropagation()
 })
 
 
-// When you use the scroll wheel within a div, its background turns orange
-let divSelector = document.querySelectorAll('div');
-divSelector.addEventListener('wheel', (event) => {event.target.style.backgroundColor = 'orange'})
+// When you use the scroll wheel within the first div, its background turns orange
+let pTag = document.querySelectorAll('p')[0];
+
+pTag.addEventListener('mouseover', function(event) {
+  window.alert('Wasup') 
+event.stopPropagation()
+})
+
+//Focus and Blur effects for each form field
+let allInputFields = document.querySelectorAll('input[type="text"]')
+inputFieldArray = Array.from(allInputFields);
+
+
+  inputFieldArray.forEach(function(inputField) {
+    inputField.addEventListener('focus', function(){
+      event.target.style.background = 'orange'; 
+    })
+  })
+
+  inputFieldArray.forEach(function(inputField) {
+    inputField.addEventListener('blur', function(){
+      event.target.style.background = 'lightgreen'; 
+    })
+  });
+
+
+  // secBorder = document.querySelectorAll('.destination');
+
+  // secBorder.forEach(function(border){
+  //   inputField.addEventListener('load', function(){
+  //     border.target.style.border = '1px solid orange';
+  // })
+
+  window.addEventListener('load', function(event) {
+    alert('Every time you refresh me a baby seal dies');
+    event.stopPropagation();
+});
+
 
 
 
@@ -48,18 +89,3 @@ divSelector.addEventListener('wheel', (event) => {event.target.style.backgroundC
 // element.addEventListener('wheel', CallBack);
 // element.addEventListener('drag / drop', CallBack);
 // element.addEventListener('load', CallBack);
-// element.addEventListener('focus', CallBack);
-// element.addEventListener('resize', CallBack);
-// element.addEventListener('scroll', CallBack);
-// element.addEventListener('select', CallBack);
-
-// // Callback functions
-// function greenColor(){
-//   p.style.color = 'green';
-// }
-
-// function fullScreen (event){
-//   if(event.type == 'fullscreenchange'){
-//     body.style.color = 'blue';
-//   }
-// 
